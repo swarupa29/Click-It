7adde729dfff0d8b01e131023cf3b816c6f0cca9
Suvigya Jain <suvigyajain1@gmail.com>
2022-03-11 18:56:31 +0530

    file upload handler added

--- evaluation/src/main/java/com/yae/evaluation/controller/SubmissionController.java
+++ evaluation/src/main/java/com/yae/evaluation/controller/SubmissionController.java
@@ -4,12 +4,15 @@ import com.yae.evaluation.entity.Submission;
 import com.yae.evaluation.service.SubmissionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/")
@@ -27,4 +30,10 @@ public class SubmissionController {
     Submission saveSubmission(@RequestBody Submission s){
         return submissionService.saveSubmission(s);
     } 
+
+    @PostMapping(value="upload")
+    public ResponseEntity uploadSubmission(@RequestParam MultipartFile file) {
+        return submissionService.uploadSubmission(file);
+    }
+    
 }
--- evaluation/src/main/java/com/yae/evaluation/service/SubmissionService.java
+++ evaluation/src/main/java/com/yae/evaluation/service/SubmissionService.java
@@ -1,13 +1,29 @@
 package com.yae.evaluation.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.swing.filechooser.FileView;
+
 import com.yae.evaluation.entity.Submission;
 import com.yae.evaluation.repository.SubmissionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 @Service
 public class SubmissionService {
+    private String USER= System.getProperty("user.name");
+    private final String  FILE_BASE_PATH="/home/"+USER+"/yae";
 
     @Autowired
     private SubmissionRepository submissionRepository;
@@ -18,6 +34,34 @@ public class SubmissionService {
 
     public Submission saveSubmission(Submission s) {
         return submissionRepository.save(s);
+    }
+
+    public ResponseEntity uploadSubmission(MultipartFile file) {
+
+        
+
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+
+        // path where uploaded file will be stored
+        Path path = Paths.get(FILE_BASE_PATH, filename);
+        File f = new File(FILE_BASE_PATH);
+        f.mkdirs();
+
+        // copies file to the path, replacing if same named file exists. TODO: Versioning  
+        try {
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        // returns a uri for the user to download the file they uploaded (for confirmation)
+        // NOTE: the path needs to have a controller which enables users to download file.
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/files/downloads/")
+            .path(filename)
+            .toUriString();
+        
+        return ResponseEntity.ok(fileDownloadUri);
     }
     
 }
